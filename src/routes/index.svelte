02a097f07c891a8348b8095bc3bedd4de9e3feae
VladimirHumeniuk <vg6962@gmail.com>
2020-02-07 15:58:47 +0100

    fixed styles

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,5 +1,5 @@
 <style>
-  h1, figure, p {
+  h1, p {
     text-align: center;
     margin: 0 auto;
   }
@@ -11,23 +11,13 @@
     margin: 0 0 0.5em 0;
   }
 
-  figure {
-    margin: 0 0 1em 0;
-  }
-
-  img {
-    width: 100%;
-    max-width: 400px;
-    margin: 0 0 1em 0;
-  }
-
   p {
     margin: 1em auto;
   }
 
   @media (min-width: 480px) {
     h1 {
-      font-size: 4em;
+      font-size: 2em;
     }
   }
 </style>
--- src/service-worker.js
+++ src/service-worker.js
@@ -58,7 +58,7 @@ self.addEventListener('fetch', event => {
   }
   */
 
-  if (event.request.cache === 'only-if-cached') return;
+  // if (event.request.cache === 'only-if-cached') return;
 
   // for everything else, try the network first, falling back to
   // cache if the user is offline. (If the pages never change, you
